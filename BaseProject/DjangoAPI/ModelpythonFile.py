f9f873afecdde413f586a569cf8cf94ba44ef9e5
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2023-09-27 17:48:53 +0530

    changed file list

--- BaseProject/DjangoAPI/ModelpythonFile.py
+++ BaseProject/DjangoAPI/ModelpythonFile.py
@@ -57,7 +57,7 @@ def train(filenames):
 
 def search(testFeature):
     feature_list = pickle.load(open('/home/f20200697/features.pickle', 'rb'))
-    filenames = pickle.load(open('/Reverse_Image_Search_s3.ipynbfilenames-caltech101.pickle', 'rb'))
+    filenames = pickle.load(open('./Reverse_Image_Search_s3.ipynbfilenames-caltech101.pickle', 'rb'))
     neighbors = NearestNeighbors(n_neighbors=5, algorithm='brute',metric='euclidean').fit(feature_list)
     distances, indices = neighbors.kneighbors([testFeature])
     # print(len(feature_list[0]))
